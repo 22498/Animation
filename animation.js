a0f8e00dc4feaeec90c9d0dcec42d3865f92f85a
22498 <22498@ma-web.nl>
2017-12-17 19:24:11 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,13 @@
+let menuItems = [];
+for (let i = 1; i <= 5; i++) {
+    menuItems[i] = document.querySelector('#box' + i);
+}
+
+
+const slide = () => {
+    let button = document.querySelector('#button-text');
+    button.style.transform += "rotate(180deg)";
+    for (let i = 1; i <= 5; i++) {
+        menuItems[i].classList.toggle('box--slide-out');
+    }
+}
